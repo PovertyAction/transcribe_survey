31ad97e50d29494b67cc4bda1c29ec6ba5498440
zdgroff <zgroff@poverty-action.org>
2016-06-16 17:43:54 -0400

    Update Program to Repeat Questions in Repeat Groups

--- transcribe_survey.py
+++ transcribe_survey.py
@@ -2,13 +2,16 @@
 #Specify variables
 #*****************
 
-#Specify name of document to import in quotes
+#Specify name of document to import in quotes.
 excelname='CEA_CU_2016_v3.xlsx'
 
-#Specify name to save document as in quotes
+#Specify name to save document as in quotes.
 wordname='CEA_CU_2016_v3.docx'
 
-#Specify title of survey if different from "form_title"
+#Specify the default number of repeat groups.
+defaultrc=11
+
+#Specify title of survey if different from "form_title."
 formtitle='CEA'
 
 #Import docx program
@@ -48,30 +51,30 @@ intro2=document.add_paragraph('Device ID:____________________               Subs
 intro3=document.add_paragraph('Sim ID (Serial):____________________    Device phone number:____________________')
 
 #Define printing function for questions not in a repeat group.
-def QuestionState():
+def QuestionState(query, tip, sort, chart, colnum):
     global number
     global newcol
     number += 1
     if tableyesno==0:
-        qp=document.add_paragraph(str(number)+'. '+question)
+        qp=document.add_paragraph(str(number)+'. '+query)
         qp.paragraph_format.space_after = Pt(0)
         qp.paragraph_format.space_before = Pt(12)
-        if hint is not None:
+        if tip is not None:
             qph=document.add_paragraph('')
-            qph.add_run(hint).italic = True
+            qph.add_run(tip).italic = True
     else:
-        if hint!=None:
-            fullquestion=question+' ('+hint+')'
+        if tip!=None:
+            fullquestion=query+' ('+tip+')'
         else:
-            fullquestion=question
-        if type.partition(' ')[0]=='select_one':
-            table.cell(0, newcol).text=str(number)+'. SELECT ONE ('+type.partition(' ')[2]+'): '+fullquestion
-        elif type.partition(' ')[0]=='select_multiple':
-            table.cell(0, newcol).text=str(number)+'. SELECT MULTIPLE ('+type.partition(' ')[2]+'): '+fullquestion
+            fullquestion=query
+        if sort.partition(' ')[0]=='select_one':
+            chart.cell(0, colnum).text=str(number)+'. SELECT ONE ('+sort.partition(' ')[2]+'): '+fullquestion
+        elif sort.partition(' ')[0]=='select_multiple':
+            chart.cell(0, colnum).text=str(number)+'. SELECT MULTIPLE ('+sort.partition(' ')[2]+'): '+fullquestion
         else:
-            table.cell(0, newcol).text=str(number)+'. '+fullquestion
+            chart.cell(0, colnum).text=str(number)+'. '+fullquestion
         
-def OptionList():
+def OptionList(paths, category):
     #global optnum
     #optnum += 1
     #print optnum
@@ -83,8 +86,8 @@ def OptionList():
         list_name=choices['A'+str(y)].value
         name=choices['B'+str(y)].value
         label=choices['C'+str(y)].value
-        if list_name==options:
-            if choicetype=='select_multiple':
+        if list_name==paths:
+            if category=='select_multiple':
                 op=document.add_paragraph('__ '+str(name)+' - '+str(label))
             else:
                 op=document.add_paragraph('[] '+str(name)+' - '+str(label))
@@ -105,95 +108,157 @@ def TableTime(repeatgroup):
         if survey['A'+str(x)].value=='end repeat':
             return tabletime
 
+#Generate function that scans a phrase and replaces references to earlier fields.
+def ReplaceRefs(phrase, mode):
+    tempphrase=phrase
+    if '${' in tempphrase and '}' in tempphrase:
+        referring=0
+        ref=''
+        for n in range(2, len(tempphrase)):
+            if tempphrase[n]=='}':
+                referring=0
+                if ref in qnumbers and mode=='Q':
+                    tempphrase=tempphrase.replace('${'+ref+'}', ' _________ (Answer to Q'+str(qnumbers[ref])+') ')
+                ref=''
+            if referring==1:
+                ref=ref+tempphrase[n]
+            if tempphrase[n-1]=='$' and tempphrase[n]=='{':
+                referring=1
+    return tempphrase     
+
 #Row by Row
 tableyesno=0
 number=0
-for x in range(8, survey.max_row):
-    print number
-    type=survey['A'+str(x)].value
-    print type
-    question=survey['C'+str(x)].value
-    hint=survey['D'+str(x)].value
+qnumbers={}
+repeat=0
+def Program(a, b):
+    for x in range(a, b):
+        global tableyesno
+        global repeat
+        print number
+        type=survey['A'+str(x)].value
+        programmed=type in ['text', 'integer', 'geopoint'] or type.partition(' ')[0] in ['select_one', 'select_multiple']
+        qnumbers[survey['B'+str(x)].value]=number
+        question=survey['C'+str(x)].value
+        print question
+        if question!=None and programmed:
+            question=ReplaceRefs(question, 'Q')
+        print question
 
-    if type=='begin repeat':
-        document.add_heading(question, 2)
-        tableyesno=TableTime(survey['B'+str(x)].value)
-        if tableyesno==1:
-            rowcount=11
-            table=document.add_table(rows=rowcount, cols=0)
-            table.style='TableGrid'
-            table.autofit=False
-            table.add_column(180000)
-            table.cell(0, 0).text='#'
-            for n in range(1, rowcount):
-                table.cell(n, 0).text=str(n)+'.'
-            newcol=1
-            typedict={}
+        hint=survey['D'+str(x)].value
 
-    if type=='end repeat':
-        if tableyesno==1:
-            table.autofit=True
-            for opt in typedict:
-                options=opt
-                choicetype=typedict[options]
-                o=document.add_paragraph('')
-                o.add_run(options).underline = True
-                o.paragraph_format.space_before=Pt(12)
-                OptionList()        
-        tableyesno=0
+        if type=='begin repeat':
+            repeatcount=survey['O'+str(x)].value
+            if not isinstance(survey['O'+str(x)].value, int):
+                repeatcount=defaultrc
+            document.add_heading(question, 2)
+            repeat=repeat+1
+            check=repeat
+            d=0
+            for z in range(x, b):
+                print survey['A'+str(z)].value
+                print 'check is '+str(check)+' and repeat is '+str(repeat)
+                if survey['A'+str(z)].value=='end repeat' and check==repeat:
+                    d=z+1
+                    break
+                elif survey['A'+str(z)].value=='begin repeat' and check!=repeat:
+                    check=check+1
+                elif survey['A'+str(z)].value=='end repeat':
+                    check=check-1
+            tableyesno=TableTime(survey['B'+str(x)].value)
+            if tableyesno==1:
+                if repeatcount!=None:
+                    rowcount=repeatcount
+                else:
+                    rowcount=defaultrc
+                table=document.add_table(rows=rowcount, cols=0)
+                table.style='TableGrid'
+                table.autofit=False
+                table.add_column(180000)
+                table.cell(0, 0).text='#'
+                for n in range(1, rowcount):
+                    table.cell(n, 0).text=str(n)+'.'
+                newcol=1
+                typedict={}
 
-    if tableyesno==0:
-        if type=='begin group':
-            document.add_heading(question, 1)
-        if type=='text':
-            QuestionState()
-            ans=document.add_paragraph('_________________________________________________________________________________________________________')
-            ans.paragraph_format.space_before=Pt(6)
-        if type=='integer':
-            QuestionState()
-            ans=document.add_paragraph('__________________________')
-            ans.paragraph_format.space_before=Pt(6)
-        if type.partition(' ')[0]=='select_one':
-            question=question+' (select one)'
-            QuestionState()
-            choicetype=type.partition(' ')[0]
-            options=type.partition(' ')[2]
-            OptionList()
-        if type.partition(' ')[0]=='select_multiple':
-            question=question+' (select multiple)'
-            QuestionState()
-            choicetype=type.partition(' ')[0]
-            options=type.partition(' ')[2]
-            OptionList()
-        if type=='note' and '${' not in question:
-            QuestionState()
-        if type=='geopoint':
-            QuestionState()
-            ans=document.add_paragraph("Latitude:  __  __* __' __\"")
-            ans=document.add_paragraph("Longitude: __  __* __' __\"")
-            ans=document.add_paragraph("Altitude:  ______m")
-            ans=document.add_paragraph("Accuracy:  ______m")
-    else:
-        if type=='text':
-            table.add_column(914400)
-            QuestionState()
-        if type=='integer':
-            table.add_column(360000)
-            QuestionState()
-        if type.partition(' ')[0]=='select_one':
-            table.add_column(914400)
-            QuestionState()
-            typedict[type.partition(' ')[2]]='select_one'
-        if type.partition(' ')[0]=='select_multiple':
-            table.add_column(914400)
-            QuestionState()
-            typedict[type.partition(' ')[2]]='select_multiple'
-        if type=='geopoint':
-            table.add_column(1600000)
-            QuestionState()
-            for n in range(1, rowcount):
-                table.cell(n, newcol).text="Latitude:  __  __* __' __\" \n Longitude: __  __* __' __\" \n Altitude:  ______m \n Accuracy:  ______m"
-        if type in ['text', 'integer', 'geopoint'] or type.partition(' ')[0] in ['select_one', 'select_multiple']:
-            newcol=newcol+1
+        if type=='end repeat':
+            repeat=repeat-1
+            if tableyesno==1:
+                table.autofit=True
+                for opt in typedict:
+                    options=opt
+                    choicetype=typedict[options]
+                    o=document.add_paragraph('')
+                    o.add_run(options).underline = True
+                    o.paragraph_format.space_before=Pt(12)
+                    OptionList(options, choicetype)        
+            tableyesno=0
+
+        if tableyesno==0:
+            if type=='begin group':
+                document.add_heading(question, 1)
+            if type=='text':
+                QuestionState(question, hint, type, '', '')
+                ans=document.add_paragraph('_________________________________________________________________________________________________________')
+                ans.paragraph_format.space_before=Pt(6)
+            if type=='integer':
+                QuestionState(question, hint, type, '', '')
+                ans=document.add_paragraph('__________________________')
+                ans.paragraph_format.space_before=Pt(6)
+            if type.partition(' ')[0]=='select_one':
+                question=question+' (select one)'
+                QuestionState(question, hint, type, '', '')
+                choicetype=type.partition(' ')[0]
+                options=type.partition(' ')[2]
+                OptionList(options, choicetype)
+            if type.partition(' ')[0]=='select_multiple':
+                question=question+' (select multiple)'
+                QuestionState(question, hint, type, '', '')
+                choicetype=type.partition(' ')[0]
+                options=type.partition(' ')[2]
+                OptionList(options, choicetype)
+            if type=='note' and '${' not in question:
+                QuestionState(question, hint, type, '', '')
+            if type=='geopoint':
+                QuestionState(question, hint, type, '', '')
+                ans=document.add_paragraph("Latitude:  __  __* __' __\"")
+                ans=document.add_paragraph("Longitude: __  __* __' __\"")
+                ans=document.add_paragraph("Altitude:  ______m")
+                ans=document.add_paragraph("Accuracy:  ______m")
+        else:
+            if type=='begin group':
+                table.add_column(914400)
+                table.cell(0, newcol).text='BEGIN GROUP: '+question
+            if type=='end group':
+                table.add_column(914400)
+                table.cell(0, newcol).text='END GROUP'
+            if type=='text' or type=='note':
+                table.add_column(914400)
+                QuestionState(question, hint, type, table, newcol)
+            if type=='integer':
+                table.add_column(360000)
+                QuestionState(question, hint, type, table, newcol)
+            if type.partition(' ')[0]=='select_one':
+                table.add_column(914400)
+                QuestionState(question, hint, type, table, newcol)
+                typedict[type.partition(' ')[2]]='select_one'
+            if type.partition(' ')[0]=='select_multiple':
+                table.add_column(914400)
+                QuestionState(question, hint, type, table, newcol)
+                typedict[type.partition(' ')[2]]='select_multiple'
+            if type=='geopoint':
+                table.add_column(1600000)
+                QuestionState(question, hint, type, table, newcol)
+                for n in range(1, rowcount):
+                    table.cell(n, newcol).text="Latitude:  __  __* __' __\" \n Longitude: __  __* __' __\" \n Altitude:  ______m \n Accuracy:  ______m"
+            if programmed:
+                newcol=newcol+1
+                
+        if repeat==1:
+            c=x
+            for i in range(0, repeatcount):
+                Program(c, d)
+
+Program(8, survey.max_row)
             
 document.save(wordname)
